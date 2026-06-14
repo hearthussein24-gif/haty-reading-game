e1da5fc731aebe7463ccaf8b9ae480054ba4dd67
hearthussein24-gif <hearthussein24@gmail.com>
2026-06-14 22:29:08 +0300

    Create home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'level_select_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Haty Reading Game'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to the Reading Game!',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LevelSelectScreen(),
+                  ),
+                );
+              },
+              child: const Text('Start Game'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
